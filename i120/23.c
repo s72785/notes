5cbf2b43970009eba5bffbad4de7989c8bf1abe4
vv01f <vv01f@debwatch>
2014-11-25 14:59:29 +0100

    23: string-arrays noch ungeklärt

--- i120/23.c
+++ i120/23.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<string.h>
-#def MAX 20
+#define MAX 20
     float sparbrief(float b, float a[], int s){
       int i;
       for(i=0; i<s; i++){
@@ -8,7 +8,7 @@
       }
       return b;
     }
-    float besterendite(float b, float z[],char banken[], int l, int c, float* max, char* bbank){
+    float besterendite(float b, float z[],char banken[][20], int l, int c, float* max, char* bbank){
       int i,mac;
       float r[3];
       float br=0.0;
@@ -19,16 +19,15 @@
           mac = i;
         }
       }
-      strcpy(bbank, banken[i]=//, (strlen(banken[i])>(MAX-1)?(MAX-1)):strlen(banken[i]));
+      strcpy(bbank, banken[i]);//, ( strlen(banken[i]) > (MAX-1) ) ? (MAX-1) : strlen(banken[i]) );
       return *max;
     }
     int main(void){
       //Banken
-      char c[][6]={
-        "Bank1",
-        "Bank2",
-        "Bank3"
-      };
+      char c[3][MAX];
+      strcpy(c[0],"Bank1");
+      strcpy(c[1],"Bank2");
+      strcpy(c[2],"Bank3");
       //Zinsen
       float a[3][4]={
         {0.2, 0.3, 0.5, 0.6},
@@ -48,7 +47,7 @@
         r[i]=sparbrief(v, a[i], s);
         printf("%d. Anlage-Betrag bei %s %8.2f ist nach %d Perioden %8.2f\n", (i+1), c[i], v, s, r[i]);
       }
-	  besterendite(v, *a, *c, h, s, &br, bestebank);
+      besterendite(v, *a, c, h, s, &br, bestebank);
       printf("bei %s\n", bestebank); 
       return 0;
     }
