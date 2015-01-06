925047c06ee175a82a8fa97e5230f28f38b72348
s72785 <s72785@users.noreply.github.com>
2015-01-06 08:43:24 +0100

    Update stack.h
    limiting .h to a generic header file, blocks moved to .c file
    stack.h + stack.c = module for custom "class" stack

--- i120/stack.h
+++ i120/stack.h
@@ -4,33 +4,8 @@ typedef struct stack_t {
 	stack_t next;	// pointer on next element on stack (recursive)
 } stack;	// name of data type
 
-int stack_push(stack **this, int item){
-	stack *first = (stack*) malloc(sizeof(stack)); //declare new first element and allocate memory sufficient for a stack element
-	if(!first) {
-		//printf("stack_push: out of memory\n");
-		return 1;//abort();
-	}
-	first->item = item;
-	first->next = *this;
-	*this = first;
-	return 0;
-}
+int stack_push(stack **this, int item);
 
-int stack_pop(stack **this){	//"pop" first element from stack
-	assert(*this != NULL);	//test *this
-	stack *first = *this;	//temp. var for freeing memory
-	int i = (*first)->item; //using first instead of this to clarify context
-	*this = (*first)->next;	//giving back remaining stack without first element
-	free(first);	//free memory for former element
-	return i;	//return value of former first element
-}
+int stack_pop(stack **this);
 
-int stack_isempty(stack **this){
-	return *this == NULL;
-/*
-	if(!*s) {	// *s == NULL
-		return 1;	//stack is empty
-	}
-	return 0;
-*/
-}
+int stack_isempty(stack **this);
