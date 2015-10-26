58b476df23d6b7c65cf464652e608971e5b86bf5
s72785 <s72785@mobile.workstation>
2015-10-26 21:38:50 +0100

    using sed

--- EwA/get.sh
+++ EwA/get.sh
@@ -1,1 +1,3 @@
+wget -c http://iasp2.informatik.htw-dresden.de/wiedem/lehre_ewa.html
+sed -i -e "s/\"fileadmin\//\"http\:\/\/iasp2\.informatik\.htw\-dresden\.de\/wiedem\/fileadmin\//g" *.html
 cat *.html|grep -o "href=.*" | cut -d'"' -f2|grep -e "pdf$" -e "doc$" -e "zip$"|sort|uniq| xargs wget -c 
